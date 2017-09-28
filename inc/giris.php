ad71173078796bd2fdd1a62dbc2a4a92191c700e
birincisait21 <birincisait21@gmail.com>
2017-09-28 22:54:23 +0300

    first commit

---
+++ inc/giris.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+</html>
+
+<?php
+include 'ayar.php';
+
+if ($_POST) {
+
+    $username = trim($_POST["username"]);
+    $sifre = trim(md5($_POST["password"]));
+
+    if (!$username || !$sifre) {
+
+        ?>
+        <script>
+            alert('kullancı adı ve sifre bos bırakılamaz ...');
+            window.location.href = '../index.php';
+        </script>
+        <?php
+
+    } else {
+
+        $uye = $db->prepare("select * from medrese_ogrenci where username = ? and password = ?");
+        $uye->execute(array($username, $sifre));
+        $z = $uye->fetch(PDO::FETCH_ASSOC);
+        $x = $uye->rowCount();
+
+        if ($x) {
+
+            $_SESSION["uye"] = $z["username"];
+            $_SESSION["eposta"] = $z["email"];
+            $_SESSION["rutbe"] = $z["rutbe"];
+            $_SESSION["id"] = $z["id"];
+            $_SESSION['user_session'] = $z["id"];
+
+            ?>
+            <script>
+                alert('Başarıyla giriş yaptınız ...');
+                window.location.href = '../index.php';
+            </script>
+            <?php
+
+        } else {
+
+            ?>
+            <script>
+                alert('Üye adı ya da şifreniz yanlıs ...');
+                window.location.href = '../index.php';
+            </script>
+            <?php
+
+        }
+    }
+
+} else {
+    ?>
+    <script>
+        alert('Buraya bu şekilde gelemezsin kusura bakma ...');
+        window.location.href = '../index.php';
+    </script>
+    <?php
+}
---
+++ inc/kayit.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+</html>
+
+<?php
+include 'ayar.php';
+
+if ($_POST) {
+
+    $isim = strip_tags(trim($_POST["name"]));
+    $soyisim = strip_tags(trim($_POST["surname"]));
+    $sifre = strip_tags(trim($_POST["password"]));
+    $email = trim($_POST["email"]);
+    $kullaniciadi = strip_tags(trim($_POST["username"]));
+
+    if (!$isim || !$sifre || !$email || !$soyisim || !$kullaniciadi) {
+
+        ?>
+        <script>
+            alert('Bütün alanları doldurmanız gerekiyor ...');
+            window.location.href = '../index.php';
+        </script>
+        <?php
+
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        ?>
+        <script>
+            alert('Bu mail adresi gecerli değil baska bir tane deneyin ...');
+            window.location.href = '../index.php';
+        </script>
+        <?php
+
+    } else {
+
+        $sifremd5 = md5($sifre);
+
+        $v = $db->prepare("select * from medrese_ogrenci where username = ?");
+        $v->execute(array($kullaniciadi));
+        $x = $v->fetch(PDO::FETCH_ASSOC);
+        $y = $v->rowCount();
+
+        if ($y) {
+
+            ?>
+            <script>
+                alert('Hata!! <?php echo $kullaniciadi?> kullanıcı adlı üye sistemde zaten var başka bir tane deneyin ...');
+                window.location.href = '../index.php';
+            </script>
+            <?php
+
+        } else {
+
+            $x = $db->prepare("insert into medrese_ogrenci set
+
+				          name = ?,
+						  surname = ?,
+						  email = ?,
+						  username = ?,
+						  password = ?
+
+				");
+
+            $kayit = $x->execute(array($isim, $soyisim, $email, $kullaniciadi, $sifremd5));
+
+            if ($kayit) {
+
+                ?>
+                <script>
+                    alert('Başarıyla Eklendi ...');
+                    window.location.href = '../index.php';
+                </script>
+                <?php
+
+            } else {
+
+                echo '<div class="hata">uye olurken bir hata olustu</div>';
+
+            }
+
+
+        }
+
+    }
+
+
+} else {
+    ?>
+    <script>
+        alert('Buraya bu şekilde gelemezsin kusura bakma ...');
+        window.location.href = '../index.php';
+    </script>
+    <?php
+}
